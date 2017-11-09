a20cb3d49f3720a488f176c21c6ce164b04c9d9c
Kalianov Oleksii <kalianov@gmail.com>
2017-11-09 18:11:34 +0200

    Second commit

---
+++ js/nav.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.mobi$(document).ready(function(){\n' +
+        '    $(\'.mobile-menu\').on(\'click\', function(){\n' +
+        '        $(\'#menu\').stop().slideToggle();\n' +
+        '    })\n' +
+        '});le-menu').on('click', function(){
+        $('#menu').stop().slideToggle();
+    })
+});
